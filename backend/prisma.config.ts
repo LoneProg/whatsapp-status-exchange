714c0d2d939910c5a6b09eb04fd772d22162129a
hazey <hazey@DESKTOP-EUHP70T.localdomain>
2026-05-08 17:26:55 +0100

    bugfix:disbled prisma rom requiring env at build time

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, env } from "prisma/config"
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
+  // datasource: {
+  //   url: env("DATABASE_URL"),
+  // },
 })
